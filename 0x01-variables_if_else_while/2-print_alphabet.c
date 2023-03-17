888208176deb3f8070c6e52474ae512bb2e1b790
khaoula-maa <k.maarouri11@gmail.com>
2023-03-17 04:38:30 -0700

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -8,14 +10,11 @@
 
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char low;
 
-	for (i = 0; i < 26; i++)
-	{	
-		putchar(alp[i]);
-	}
-	putchar('\n');
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
 
 	return (0);
 }
